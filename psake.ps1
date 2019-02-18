adf70a060cccfa1eeac1d9d68581045dbdb5ac39
= <matthewdavis111@gmail.com>
2019-02-18 15:54:33 -0500

    added module test

--- psake.ps1
+++ psake.ps1
@@ -4,3 +4,13 @@ task Analyse {
     'Running analyzer'
     Invoke-ScriptAnalyzer -Path .\DotNetCoreReleaseTool\private\Get-Data.ps1 -Verbose
 }
+
+task ModuleTest {
+    'Running Module Test'
+    Invoke-Pester -Path $PSScriptRoot\Tests
+}
+
+task UnitTest {
+    'Running Unit Tests'
+    Invoke-Pester -Path $PSScriptRoot\Tests\Unit
+}
