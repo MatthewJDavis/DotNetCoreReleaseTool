4c2658a8ffd7da6da35981c4215f5bd559b145cc
= <matthewdavis111@gmail.com>
2019-02-18 15:54:22 -0500

    fixed mock data

--- Tests/Unit/Get-DNCLatestVersion.Tests.ps1
+++ Tests/Unit/Get-DNCLatestVersion.Tests.ps1
@@ -12,7 +12,7 @@ Describe 'Get-DNCLatestVersion' {
     }
     Context 'Output' {
         Mock Get-Data -MockWith { [PSCustomObject]@{ 
-                'version-runtime' = '2..2'
+                'version-runtime' = '2.2'
                 'version-sdk'     = '2.2.104'
                 'date'            = '2019-02-18'
                 'lts-runtime'     = 'false'
