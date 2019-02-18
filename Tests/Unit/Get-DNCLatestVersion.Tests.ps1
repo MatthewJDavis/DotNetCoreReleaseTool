d7482667666ff0883fa82b287a18e53b9e8b8ea3
= <matthewdavis111@gmail.com>
2019-02-18 15:44:27 -0500

    added unit test

---
+++ Tests/Unit/Get-DNCLatestVersion.Tests.ps1
@@ -0,0 +1,27 @@
+# Get the function from the Public directory and dot source it for testing against
+$projectRoot = Resolve-Path "$PSScriptRoot\..\.."
+$PublicRoot = Resolve-Path "$projectRoot\*\Public"
+. $PublicRoot\Get-DNCLatestVersion.ps1
+
+Describe 'Get-DNCLatestVersion' {
+    $cmdlet = Get-Command Get-DNCLatestVersion 
+    Context 'Cmdlet' {
+        it "Has an output of a PSCustomObject" {
+            $cmdlet.OutputType.Name -eq 'PSCustomObject' | Should Be $true 
+        }
+    }
+    Context 'Output' {
+        Mock Get-Data -MockWith { [PSCustomObject]@{ 
+                'version-runtime' = '2..2'
+                'version-sdk'     = '2.2.104'
+                'date'            = '2019-02-18'
+                'lts-runtime'     = 'false'
+                'lts-sdk'         = 'false'   
+            } 
+        } 
+        it "returns version 2.2 when no major or minor version is specified" {
+            $latest = Get-DNCLatestVersion
+            $latest.versionRuntime -like "2.2*" | Should Be $true
+        }
+    }
+}
