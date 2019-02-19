b42602208e3255cece69465fa0c035b84416a701
= <matthewdavis111@gmail.com>
2019-02-18 20:37:05 -0500

    added psm1 from build

---
+++ DotNetCoreReleaseTool/DotNetCoreReleaseTool.psm1
@@ -0,0 +1,37 @@
+function Get-Data {
+    # Private function to return the data from the dotnet core releases json file
+    param (
+        $uri = 'https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases.json'
+    )
+       Invoke-RestMethod -Uri $uri -Method Get -ErrorAction Stop
+}
+function Get-DNCLatestVersion {
+    [CmdletBinding()]
+    [OutputType('PSCustomObject')]
+    param (
+        $MajorVersion = '2',
+        $MinorVersion = '2'
+    )
+    
+    begin {
+    }
+    
+    process {
+        $version = "$MajorVersion.$MinorVersion*"
+        $data = Get-Data 
+        $latestRelease = $data | Where-Object {$_.'version-runtime' -like $version} | Select-Object -First 1
+        [pscustomobject]@{
+            ReleaseDate    = $latestRelease.date
+            VersionRuntime = $latestRelease.'version-runtime'
+            VersionSdk     = $latestRelease.'version-sdk'
+            LtsRuntime     = $latestRelease.'lts-runtime'
+            LtsSdk         = $latestRelease.'lts-sdk'
+        }
+
+
+    }
+    
+    end {
+    }
+}
+Export-ModuleMember -Function Get-DNCLatestVersion
