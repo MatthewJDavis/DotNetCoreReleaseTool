06c082385275fad2f66bc12d328a067f42f81601
= <matthewdavis111@gmail.com>
2019-02-18 20:36:24 -0500

    updated psake script

--- psake.ps1
+++ psake.ps1
@@ -2,15 +2,25 @@ task default -depends Analyse
 
 task Analyse {
     'Running analyzer'
-    Invoke-ScriptAnalyzer -Path .\DotNetCoreReleaseTool\private\Get-Data.ps1 -Verbose
+    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\private\*.ps1 -Verbose
+    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\public\*.ps1 -Verbose
 }
 
-task ModuleTest {
+task ModuleTest -depends Analyse {
     'Running Module Test'
-    Invoke-Pester -Path $PSScriptRoot\Tests
+    Invoke-Pester -Path $PSScriptRoot\Tests\DotNetCoreReleaseTool.Tests.ps1
 }
 
 task UnitTest {
     'Running Unit Tests'
     Invoke-Pester -Path $PSScriptRoot\Tests\Unit
+}
+
+task BuildPSMFile {
+    'Running Build PSM File'
+    exec { & .\buildPSMFile.ps1}
+}
+
+task AnalysePSMFile {
+    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\DotNetCoreReleaseTool.psm1
 }
