b9b71da92e1225def2e39b891473a04a3caa0195
= <matthewdavis111@gmail.com>
2019-02-25 20:23:28 -0500

    formatting

--- Tests/Unit/Get-DNCLatestVersion.Tests.ps1
+++ Tests/Unit/Get-DNCLatestVersion.Tests.ps1
@@ -13,25 +13,26 @@ $PrivateRoot = Resolve-Path "$projectRoot\*\Private"
 
 . $PublicRoot\Get-DNCLatestVersion.ps1
 . $PrivateRoot\Get-Data.ps1
-    Describe 'Get-DNCLatestVersion' {
-        $cmdlet = Get-Command Get-DNCLatestVersion
-        Context 'Cmdlet' {
-            it "Has an output of a PSCustomObject" {
-                $cmdlet.OutputType.Name -eq 'PSCustomObject' | Should Be $true
-            }
-        }
-        Context 'Output' {
-            Mock Get-Data -MockWith { [PSCustomObject]@{
-                    'version-runtime' = '2.2'
-                    'version-sdk'     = '2.2.104'
-                    'date'            = '2019-02-18'
-                    'lts-runtime'     = 'false'
-                    'lts-sdk'         = 'false'
-                }
-            }
-            it "returns version 2.2 when no major or minor version is specified" {
-                $latest = Get-DNCLatestVersion
-                $latest.versionRuntime -like "2.2*" | Should Be $true
-            }
+
+Describe 'Get-DNCLatestVersion' {
+    $cmdlet = Get-Command Get-DNCLatestVersion
+    Context 'Cmdlet' {
+        it "Has an output of a PSCustomObject" {
+            $cmdlet.OutputType.Name -eq 'PSCustomObject' | Should Be $true
         }
     }
+    Context 'Output' {
+        Mock Get-Data -MockWith { [PSCustomObject]@{
+                'version-runtime' = '2.2'
+                'version-sdk'     = '2.2.104'
+                'date'            = '2019-02-18'
+                'lts-runtime'     = 'false'
+                'lts-sdk'         = 'false'
+            }
+        }
+        it "returns version 2.2 when no major or minor version is specified" {
+            $latest = Get-DNCLatestVersion
+            $latest.versionRuntime -like "2.2*" | Should Be $true
+        }
+    }
+}
