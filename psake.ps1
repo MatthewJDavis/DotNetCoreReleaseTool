64f4894ad37d493a8bc2dd64d7403b2cc56bc3c4
= <matthewdavis111@gmail.com>
2019-02-25 21:01:14 -0500

    added  checking for output to fail build

--- psake.ps1
+++ psake.ps1
@@ -1,19 +1,32 @@
-task default -depends Analyse
+task default -depends AnalyseFunctions
 
-task Analyse {
-    'Running analyzer'
-    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\private\*.ps1 -Verbose
-    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\public\*.ps1 -Verbose
+task AnalyseFunctions {
+    'Running Script Analyser on Private and Public functions'
+    $privateSAResults = Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\private\*.ps1 -Severity @('Error', 'Warning') -EnableExit
+    $publicSAResults += Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\public\*.ps1 -Severity @('Error', 'Warning') -EnableExit
+    if ($privateSAResults -or $publicSAResults) {
+        $privateSAResults | Format-Table
+        $publicSAResults | Format-Table
+        Write-Error -Message 'One or more script analyser errors/ warnings were found'
+    }
 }
 
-task ModuleTest -depends Analyse {
+task ModuleTest -depends AnalyseFunctions {
     'Running Module Test'
-    Invoke-Pester -Path $PSScriptRoot\Tests\DotNetCoreReleaseTool.Tests.ps1
+    $modTestResults = Invoke-Pester -Path $PSScriptRoot\Tests\DotNetCoreReleaseTool.Tests.ps1 -PassThru
+    if ($modTestResults.FailedCount -gt 0) {
+        $modTestResults | Format-Table
+        Write-Error -Message 'One or more pester tests failed.'
+    }
 }
 
 task UnitTest {
     'Running Unit Tests'
-    Invoke-Pester -Path $PSScriptRoot\Tests\Unit
+    $unitTestResults = Invoke-Pester -Path $PSScriptRoot\Tests\Unit -PassThru
+    if ($unitTestResults.FailedCount -gt 0) {
+        $unitTestResults | Format-Table
+        Write-Error -Message 'One or more pester tests failed'
+    }
 }
 
 task BuildPSMFile {
@@ -21,6 +34,15 @@ task BuildPSMFile {
     exec { & .\buildPSMFile.ps1}
 }
 
-task AnalysePSMFile {
-    Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\DotNetCoreReleaseTool.psm1
+task AnalysePSMFile -depends BuildPSMFile {
+    'Runnng script analyser on Script Module'
+    $scriptModuleSAResults = Invoke-ScriptAnalyzer -Path $PSScriptRoot\DotNetCoreReleaseTool\DotNetCoreReleaseTool.psm1 -Severity @('Error', 'Warning') -EnableExit
+    if ($scriptModuleSAResults) {
+        $scriptModuleSAResults | Format-Table
+        Write-Error 'One or more Script Analyser warning or errors were found.'
+    }
+}
+
+task PublishModule -depends AnalyseFunctions, ModuleTest, UnitTest, BuildPSMFile, AnalysePSMFile {
+    Copy-Item .\DotNetCoreReleaseTool -Destination C:\TEMP -Recurse -Force
 }
